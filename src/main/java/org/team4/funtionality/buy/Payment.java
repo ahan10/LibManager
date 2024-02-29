f9558a7a7fdf71821529fa658ed1afb5d3d177b6
ahan10 <ahanbhargava10@gmail.com>
2024-02-29 00:55:07 -0500

    [ADD] Added payment classes.

---
+++ src/main/java/org/team4/funtionality/buy/Payment.java
@@ -0,0 +1,9 @@
+package org.team4.funtionality.buy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public interface Payment {
+    public static final ArrayList<String> MODE = new ArrayList<>(Arrays.asList("Credit Card", "Debit Card", "Mobile Wallet", "PayPal"));
+    
+}
---
+++ src/main/java/org/team4/funtionality/buy/Purcahse.java
@@ -0,0 +1,36 @@
+package org.team4.funtionality.buy;
+
+import org.team4.model.items.Item;
+import org.team4.model.user.User;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Purcahse implements Payment{
+    private long purchaseID;
+    private Date purchaseDate;
+    private double amount;
+    private User user;
+    private Item item;
+    private String mode;
+
+    public Purcahse(double amount, User user, Item item, String mode) {
+        this.purchaseID = new Random().nextLong();
+        this.purchaseDate = new Date();
+        this.amount = amount;
+        this.user = user;
+        this.item = item;
+        this.mode = mode;
+    }
+
+    public boolean processPayment(String mode){
+        return MODE.contains(mode);
+    }
+
+    public Purcahse getPayment(){
+        if(this.processPayment(this.mode)){
+            return this;
+        }
+        return null;
+    }
+}
