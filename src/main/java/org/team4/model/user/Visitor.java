fb9ea323b865499d28bd857a19a129c58d905452
ahan10 <ahanbhargava10@gmail.com>
2024-02-29 00:14:41 -0500

    [ADD] Added Visitor.java

---
+++ src/main/java/org/team4/model/user/Visitor.java
@@ -0,0 +1,7 @@
+package org.team4.model.user;
+
+public class Visitor extends User{
+    public Visitor(String email, String password, String name, String type) {
+        super(email, password, name, type);
+    }
+}
