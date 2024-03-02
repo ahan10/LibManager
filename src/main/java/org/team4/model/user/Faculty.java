7450e0440e8f5990e5fbcd1505492ea239899b86
ahan10 <ahanbhargava10@gmail.com>
2024-03-01 22:38:18 -0500

    [ADD] Added constructors to only be used by MaintainUser.java

--- src/main/java/org/team4/model/user/Faculty.java
+++ src/main/java/org/team4/model/user/Faculty.java
@@ -16,6 +16,11 @@ public class Faculty extends User {
         super(email, password, name, type);
     }
 
+    //ONLY TO BE USED BY MAINTAIN USER
+    public Faculty(String email, String password, String name, String type, boolean validated) {
+        super(email, password, name, type, validated);
+    }
+
     public long getFacultyID() {
         return facultyID;
     }
--- src/main/java/org/team4/model/user/Visitor.java
+++ src/main/java/org/team4/model/user/Visitor.java
@@ -4,4 +4,9 @@ public class Visitor extends User{
     public Visitor(String email, String password, String name, String type) {
         super(email, password, name, type);
     }
+
+    //ONLY TO BE USED BY MAINTAIN USER
+    public Visitor(String email, String password, String name, String type, boolean validated) {
+        super(email, password, name, type, validated);
+    }
 }
