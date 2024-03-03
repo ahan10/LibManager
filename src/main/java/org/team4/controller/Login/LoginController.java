568cb676aa5a81e2aaefaee89a4dc530fbdb1fbf
s-w6 <16546401+s-w6@users.noreply.github.com>
2024-03-02 22:47:40 -0500

    login and registration complete

--- src/main/java/org/team4/controller/Login/LoginController.java
+++ src/main/java/org/team4/controller/Login/LoginController.java
@@ -3,22 +3,53 @@ package org.team4.controller.Login;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import org.team4.controller.managerdashboard.ManagerController;
 import org.team4.maintaindb.MaintainUser;
 import org.team4.model.user.User;
 import org.team4.view.Login.LoginPage;
 import org.team4.view.Manager.ManagerDashboard;
 
-public class LoginController implements ActionListener{
+public class LoginController implements ActionListener {
 	private LoginPage loginPage;
 	private MaintainUser maintainUser;
-	
+
 	public LoginController(LoginPage loginPage, MaintainUser maintainUser) {
 		this.loginPage = loginPage;
 		this.maintainUser = maintainUser;
 		addListeners();
 	}
-	
+
+	/**
+	 * Checks if password contains at least 1 upper case letter, lower case letter,
+	 * number, and symbol
+	 * 
+	 * @param password
+	 * @return
+	 */
+	private boolean checkPasswordStrength(String password) {
+		char ch;
+		boolean upperCaseFlag = false;
+		boolean lowerCaseFlag = false;
+		boolean numberFlag = false;
+		boolean symbolFlag = false;
+		for (int i = 0; i < password.length(); i++) {
+			ch = password.charAt(i);
+			if (Character.isDigit(ch)) {
+				numberFlag = true;
+			} else if (Character.isUpperCase(ch)) {
+				upperCaseFlag = true;
+			} else if (Character.isLowerCase(ch)) {
+				lowerCaseFlag = true;
+			} else {
+				symbolFlag = true;
+			}
+		}
+
+		return upperCaseFlag & lowerCaseFlag & numberFlag & symbolFlag;
+	}
+
 	private void addListeners() {
 		loginPage.getLoginButton().addActionListener(this);
 		loginPage.getRegisterButton().addActionListener(this);
@@ -54,7 +85,7 @@ public class LoginController implements ActionListener{
 							ManagerDashboard frame = new ManagerDashboard();
 							frame.setLocationRelativeTo(null);
 							frame.setVisible(true);
-							ManagerController controller = new ManagerController(frame);
+							@SuppressWarnings("unused") ManagerController controller = new ManagerController(frame);
 							loginPage.setVisible(false);
 							break;
 						case "VISITOR":
@@ -75,32 +106,52 @@ public class LoginController implements ActionListener{
 				System.out.println("Account not found\nPlease make sure you have entered the correct Email");
 			}
 		}
-		
+
 		else if (e.getSource() == loginPage.getCreateAccountButton()) {
-			boolean flag = false;
-			for (User u : maintainUser.users) {
-				if (u.getEmail().equals(loginPage.getRegisterEmailInput())) {
-					flag = true;
-					System.out.println("Account with email " + u.getEmail() + " already exists");
+			if (loginPage.getUserNameInput().length() == 0
+					|| loginPage.getUserNameInput().equals("Name")
+					|| loginPage.getRegisterEmailInput().length() == 0
+					|| loginPage.getRegisterEmailInput().equals("Email")
+					|| loginPage.getRegisterPasswordInput().length() == 0
+					|| loginPage.getRegisterPasswordInput().equals("Password")) {
+				System.out.println("Missing Input Fields");
+				JOptionPane.showMessageDialog(loginPage,
+						"Registration Failed!\nPlease ensure all fields are filled in properly");
+			} else {
+				boolean flag = false;
+				for (User u : maintainUser.users) {
+					if (u.getEmail().equals(loginPage.getRegisterEmailInput())) {
+						flag = true;
+						System.out.println("Account with email " + u.getEmail() + " already exists");
+						JOptionPane.showMessageDialog(loginPage, "Account with email " + u.getEmail()
+								+ " already exists\nTry logging in or use a different email");
+						break;
+					}
+				}
+				if (flag == false) {
+					System.out.println("Email doesnt already exist, still need to check other credentials");
+					if (checkPasswordStrength(loginPage.getRegisterPasswordInput())) {
+						System.out.println("Password Strong");
+						maintainUser.addUser(new User(loginPage.getRegisterEmailInput(), loginPage.getRegisterPasswordInput(), loginPage.getUserNameInput(), loginPage.getRegisterAccountType()));
+						System.out.println("New User Registered");
+					} else {
+						System.out.println("Password Weak");
+						JOptionPane.showMessageDialog(loginPage,
+								"Registration Failed!\nPlease ensure password contains at least 1 uppercase letter, lowercase letter, number, and symbol");
+					}
 				}
 			}
-			if (flag == false) {
-				System.out.println("Email doesnt already exist, still need to check other credentials");
-			}
+		}
 
-			}
-		
 		else if (e.getSource() == loginPage.getRegisterButton()) {
 			System.out.println("User attempted to Register");
 			loginPage.showRegistrationPage();
 		}
-		
+
 		else if (e.getSource() == loginPage.getAlreadyHaveAccountButton()) {
 			System.out.println("User wishes to login to existing account");
 			loginPage.showLoginPage();
 		}
 
 	}
-	
-
 }
