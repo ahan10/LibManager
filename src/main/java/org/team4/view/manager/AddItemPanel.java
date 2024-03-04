967dc70faeb1cfb151ab5d8d09ae6c8b059346f6
ahan10 <ahanbhargava10@gmail.com>
2024-03-04 17:29:39 -0500

    [ADD] Updated add item panel

--- src/main/java/org/team4/view/manager/AddItemPanel.java
+++ src/main/java/org/team4/view/manager/AddItemPanel.java
@@ -18,18 +18,18 @@ public class AddItemPanel extends JPanel {
 	}
 	
 	public void initAdd() {
-		setBounds(100, 100, 788, 520);
+		setBounds(100, 100, 988, 720);
 		setLayout(new BorderLayout());
 		
 		JPanel panel = new JPanel();
-	    panel.setLayout(null);
-		
-		JLabel TitleLabel = new JLabel("Add Item");
-		TitleLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		TitleLabel.setBounds(328, 28, 132, 16);
-		add(TitleLabel);
 		
 		add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel TitleLabel = new JLabel("Add Item");
+		TitleLabel.setBounds(428, 25, 89, 25);
+		panel.add(TitleLabel);
+		TitleLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		
 	}
 
