c5ac1c9ba0e4f3ecbb1f88eb186ce6203c03a828
ahan10 <ahanbhargava10@gmail.com>
2024-03-05 16:45:17 -0500

    [HOTFIX] Fixed maintain class

--- src/main/java/org/team4/maintaindb/MaintainBooks.java
+++ src/main/java/org/team4/maintaindb/MaintainBooks.java
@@ -6,6 +6,8 @@ import org.team4.model.items.BookBuilder;
 import org.team4.model.user.User;
 
 import org.team4.model.items.Item;
+import org.team4.model.items.PurchasableItemDecorator;
+import org.team4.model.items.RentableItemDecorator;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,26 +20,36 @@ public class MaintainBooks {
 
     public MaintainBooks(){}
 
-    public static ArrayList<Book> getAllBooks(){
-        ArrayList<Book> allBooks = new ArrayList<Book>();
+    public static ArrayList<Item> getAllBooks(){
+        ArrayList<Item> allBooks = new ArrayList<>();
         CsvReader reader = null;
 
         try {
             reader = new CsvReader(FILE_PATH);
             reader.readHeaders();
             while(reader.readRecord()){
-               Book b = ((BookBuilder) new BookBuilder()
-                       .title(reader.get("title"))
-                       .yearPublished(Integer.parseInt(reader.get("yearPublished"))))
-                       .noOfPages(Integer.parseInt(reader.get("noOfPages")))
-                       .author(reader.get("author"))
-                       .ISBN(reader.get("ISBN"))
-                       .publisher(reader.get("publisherName"))
-                       .edition(Integer.parseInt(reader.get("edition")))
-                       .genre(reader.get("genre"))
-                       .build();
+                Item b = ((BookBuilder) new BookBuilder()
+                        .title(reader.get("title"))
+                        .yearPublished(Integer.parseInt(reader.get("yearPublished"))))
+                        .noOfPages(Integer.parseInt(reader.get("noOfPages")))
+                        .author(reader.get("author"))
+                        .ISBN(reader.get("ISBN"))
+                        .publisher(reader.get("publisherName"))
+                        .edition(Integer.parseInt(reader.get("edition")))
+                        .genre(reader.get("genre"))
+                        .build();
+                if(Boolean.parseBoolean(reader.get("isPurchaseable"))) {
+                    PurchasableItemDecorator pb = new PurchasableItemDecorator(b);
+                    b = pb.getItem();
+                }
+                if(Boolean.parseBoolean(reader.get("isRentable"))) {
+                    RentableItemDecorator rb = new RentableItemDecorator(b);
+                    b = rb.getItem();
 
-               allBooks.add(b);
+                }
+
+                allBooks.add(b);
+
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -48,17 +60,18 @@ public class MaintainBooks {
         return allBooks;
     }
     public static ArrayList<Book> searchBooks(String query) {
-        ArrayList<Book> books = getAllBooks();
+        ArrayList<Item> books = getAllBooks();
         ArrayList<Book> searchedBooks = new ArrayList<>();
 
-        for (Book book: books) {
-            if (book.getTitle().toLowerCase().contains(query.toLowerCase())) {
-            	searchedBooks.add(book);
+        for (Item item: books) {
+            if (item.getTitle().toLowerCase().contains(query.toLowerCase())) {
+                Book book = (Book)item;
+                searchedBooks.add(book);
             }
         }
         return searchedBooks;
     }
 
-    
-   
+
+
 }
