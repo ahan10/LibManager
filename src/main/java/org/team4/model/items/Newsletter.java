6832f4fa126ffd8d79b914b5ac94a41222db509c
John Yacoub <John Yacoub@192.168.5.2>
2024-03-06 00:33:36 -0500

    [ADD] New newsletter and newsletter builder classes

--- src/main/java/org/team4/model/items/Newsletter.java
+++ src/main/java/org/team4/model/items/Newsletter.java
@@ -1,10 +1,19 @@
 package org.team4.model.items;
 
-public class Newsletter extends Item{
-    private String publisher;
+public class Newsletter {
+	private String title;
+	private String link;
 
-    public Newsletter(Item item, String publisher) {
-        super(item);
-        this.publisher = publisher;
-    }
+	public Newsletter(String title, String link) {
+		this.title = title;
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getLink() {
+		return link;
+	}
 }
