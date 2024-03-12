661fd8ad457e36075bb223e58f677a997a76d171
yacoub-john <jyacoub@my.yorku.ca>
2024-03-12 15:18:41 -0400

    [ADD] DVD panel and DVD item panel to show dvd search results
    and each dvd

---
+++ src/main/java/org/team4/view/user/DVDItemPanel.java
@@ -0,0 +1,137 @@
+package org.team4.view.user;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.team4.model.items.DVD;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class DVDItemPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private JFrame window;
+	private DVD dvd;
+	private JLabel lblTitleValue = new JLabel();
+	private JLabel lblGenreValue = new JLabel();
+	private JLabel lblISBNValue = new JLabel();
+	private JLabel lblDurationValue = new JLabel();
+	private JLabel lblYearPublishedValue = new JLabel();
+	private JLabel lblQuantityValue = new JLabel();
+	private JLabel lblPriceValue = new JLabel();
+
+
+
+	public DVDItemPanel(JFrame window, DVD dvd) {
+		this.window = window;
+		this.dvd = dvd;
+		setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Title:");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblTitle.setBounds(20, 20, 45, 13);
+		
+		JLabel lblGenre = new JLabel("Genre:");
+		lblGenre.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblGenre.setBounds(20, 40, 45, 13);
+		
+		JLabel lblISBN = new JLabel("ISBN:");
+		lblISBN.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblISBN.setBounds(20, 60, 45, 13);
+		
+		JLabel lblYearPublished = new JLabel("Published:");
+		lblYearPublished.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblYearPublished.setBounds(20, 100, 100, 13);
+		
+		JLabel lblDuration = new JLabel("Duration: ");
+		lblDuration.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblDuration.setBounds(20, 120, 100, 13);
+		
+		JLabel lblPrice = new JLabel("Price:");
+		lblPrice.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblPrice.setBounds(20, 140, 70, 13);
+		
+		JLabel lblQuantity = new JLabel("Quantity:");
+		lblQuantity.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblQuantity.setBounds(20, 160, 70, 13);
+		
+		
+		add(lblTitle);
+		add(lblGenre);
+		add(lblISBN);
+		add(lblYearPublished);
+		add(lblDuration);
+		add(lblPrice);
+		add(lblQuantity);
+		
+
+		lblTitleValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblTitleValue.setBounds(100, 20, 200, 13);
+		
+		lblGenreValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblGenreValue.setBounds(100, 40, 200, 13);
+		
+		lblISBNValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblISBNValue.setBounds(100, 60, 200, 13);
+		
+		
+		
+		lblYearPublishedValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblYearPublishedValue.setBounds(100, 100, 200, 13);
+		
+		lblDurationValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblDurationValue.setBounds(100, 120, 200, 13);
+		
+		lblPriceValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblPriceValue.setBounds(100, 140, 200, 13);
+		
+		lblQuantityValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblQuantityValue.setBounds(100, 160, 200, 13);
+
+		
+		add(lblTitleValue);
+		add(lblGenreValue);
+		add(lblISBNValue);
+
+		add(lblYearPublishedValue);
+		add(lblDurationValue);
+		add(lblPriceValue);
+		add(lblQuantityValue);
+		
+		JButton btnBuy = new JButton("Buy");
+		btnBuy.setBounds(20, 200, 85, 21);
+		add(btnBuy);
+		
+		JButton btnRent = new JButton("Rent");
+		btnRent.setBounds(160, 200, 85, 21);
+		add(btnRent);
+		
+
+	}
+	
+	
+	public void showItemInfo() {
+		lblTitleValue.setText(dvd.getTitle());
+		lblGenreValue.setText(dvd.getGenre());
+		lblISBNValue.setText(dvd.getISBN());
+
+		lblYearPublishedValue.setText(dvd.getYearPublished() + "");
+		lblDurationValue.setText(dvd.getDuration() + " mins");
+		lblPriceValue.setText("$" + dvd.getPrice() + "");
+		lblQuantityValue.setText(dvd.getQuantity() + "");
+	}
+
+
+	public void setWindow(JFrame window) {
+		this.window = window;
+	}
+
+
+	public void setDVD(DVD dvd) {
+		this.dvd = dvd;
+	}
+	
+	public JFrame getWindow() {
+		return window;
+	}
+
+}	
--- src/main/java/org/team4/view/user/DVDResultsPanel.java
+++ src/main/java/org/team4/view/user/DVDResultsPanel.java
@@ -1,34 +1,94 @@
 package org.team4.view.user;
 
 import javax.swing.*;
+
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.Panel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+
+import org.team4.maintaindb.MaintainDVD;
+import org.team4.maintaindb.MaintainDatabase;
 import org.team4.model.items.DVD;
 import org.team4.view.user.search.DVDTableModel;
 
+
 public class DVDResultsPanel extends JPanel {
-    private static final long serialVersionUID = 1L;
-    private static JTable table;
+	private static final long serialVersionUID = 1L;
+	private static JTable table;
+	private String query;
+	private JFrame window;
 
-    public DVDResultsPanel() {
-        setBounds(100, 100, 788, 520);
-        setLayout(null);
 
-        JLabel label = new JLabel("DVD Search Results");
-        label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-        label.setBounds(324, 5, 200, 26);
-        add(label);
-    }
+	public DVDResultsPanel() {
+		setBounds(100, 100, 788, 520);
+		setLayout(null);
 
-    public DVDResultsPanel(ArrayList<DVD> results) {
-        setLayout(new BorderLayout());
-        DVDTableModel model = new DVDTableModel(results);
-        table = new JTable(model);
-        add(new JScrollPane(table), BorderLayout.CENTER);
-    }
+	}
 
-    public static JTable getTable() {
-        return table;
-    }
+	public DVDResultsPanel(String query, JFrame window) {
+
+		this.window = window;
+		this.query = query;
+
+		setBounds(100, 100, 788, 520);
+		setLayout(new BorderLayout());
+		JLabel label = new JLabel("DVD Search Results for Title: " + query);
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label.setBounds(324, 5, 239, 26);
+		add(label, BorderLayout.PAGE_START);
+
+	}
+
+
+	public void addSearchResults() {
+		ArrayList<DVD> results = MaintainDatabase.getInstance().getDVDDatabase().searchDVDs(query);
+		DVDTableModel model = new DVDTableModel(results);
+
+		if(results.isEmpty()) {
+			JOptionPane.showMessageDialog(this, "No results found.", "Search Error", JOptionPane.ERROR_MESSAGE);
+			window.dispose();
+			return;
+
+		}
+		else {
+			table = new JTable(model);
+			table.setDefaultEditor(Object.class, null);
+			add(new Panel().add(new JScrollPane(table)));
+
+			table.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					int row = table.rowAtPoint(e.getPoint());
+					int col = table.columnAtPoint(e.getPoint());
+					if (col == 0) {
+						DVD dvd = MaintainDVD.getInstance().searchExactDVDByISBN(table.getValueAt(row, 4).toString());
+						JFrame itemInfoFrame = new JFrame(dvd.getTitle());
+
+						DVDItemPanel dvdPanel = new DVDItemPanel(itemInfoFrame, dvd);
+						dvdPanel.showItemInfo();
+						itemInfoFrame.setContentPane(new JScrollPane(dvdPanel));
+						itemInfoFrame.setSize(300, 280);
+						itemInfoFrame.setVisible(true);
+					}
+				}
+			});
+
+
+			window.setContentPane(new JScrollPane(this));
+			window.setSize(1200, 900);
+			window.setVisible(true);
+		}
+
+	}
+
+	public JFrame getWindow() {
+		return window;
+	}
+
+	public static JTable getTable() {
+		return table;
+	}
 }
