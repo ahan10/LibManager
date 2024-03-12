f4d4a97c3fe95b5d2d220e9b496302f06936f904
yacoub-john <jyacoub@my.yorku.ca>
2024-03-12 15:19:13 -0400

    [ADD] Magazine panel and Magazine item panel to show Magazine search results and each Magazine

---
+++ src/main/java/org/team4/view/user/MagazineItemPanel.java
@@ -0,0 +1,124 @@
+package org.team4.view.user;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.team4.model.items.Magazine;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class MagazineItemPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private JFrame window;
+	private Magazine magazine;
+	
+	private JLabel lblTitleValue = new JLabel();
+	private JLabel lblISBNValue = new JLabel();
+	private JLabel lblYearPublishedValue = new JLabel();
+
+	private JLabel lblPublisherValue = new JLabel();
+	private JLabel lblIssueNumberValue = new JLabel();
+	private JLabel lblQuantityValue = new JLabel();
+
+
+	public MagazineItemPanel(JFrame window, Magazine magazine) {
+		this.window = window;
+		this.magazine = magazine;
+		setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Title:");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblTitle.setBounds(20, 20, 45, 13);
+		
+		JLabel lblISBN = new JLabel("ISBN:");
+		lblISBN.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblISBN.setBounds(20, 40, 45, 13);
+		
+		JLabel lblYearPublished = new JLabel("Published:");
+		lblYearPublished.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblYearPublished.setBounds(20, 60, 100, 13);
+		
+
+		JLabel lblPublisher = new JLabel("Publisher:");
+		lblPublisher.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblPublisher.setBounds(20, 100, 100, 13);
+		
+		JLabel lblIssueNumber = new JLabel("Issue Number: ");
+		lblIssueNumber.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblIssueNumber.setBounds(20, 120, 100, 13);
+		
+		JLabel lblQuantity = new JLabel("Quantity:");
+		lblQuantity.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblQuantity.setBounds(20, 140, 70, 13);
+		
+		
+		add(lblTitle);
+		add(lblISBN);
+		add(lblYearPublished);
+		
+		add(lblPublisher);
+		add(lblIssueNumber);
+		add(lblQuantity);
+		
+
+		lblTitleValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblTitleValue.setBounds(100, 20, 200, 13);
+		
+		lblISBNValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblISBNValue.setBounds(100, 40, 200, 13);
+		
+		lblYearPublishedValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblYearPublishedValue.setBounds(100, 60, 200, 13);
+		
+		lblPublisherValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblPublisherValue.setBounds(100, 100, 200, 13);
+		
+		lblIssueNumberValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblIssueNumberValue.setBounds(100, 120, 200, 13);
+		
+		lblQuantityValue.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblQuantityValue.setBounds(100, 140, 200, 13);
+
+		
+		add(lblTitleValue);
+		add(lblISBNValue);
+		add(lblYearPublishedValue);
+		
+		add(lblPublisherValue);
+		add(lblIssueNumberValue);
+		add(lblQuantityValue);
+		
+		JButton btnRent = new JButton("Rent");
+		btnRent.setBounds(90, 180, 85, 21);
+		add(btnRent);
+		
+
+	}
+	
+	
+	public void showItemInfo() {
+		lblTitleValue.setText(magazine.getTitle());
+		lblISBNValue.setText(magazine.getISBN());
+		lblYearPublishedValue.setText(magazine.getYearPublished() + "");
+		
+		lblPublisherValue.setText(magazine.getPublisher());
+		lblIssueNumberValue.setText(magazine.getIssueNumber() + "");
+		lblQuantityValue.setText(magazine.getQuantity() + "");
+	}
+
+
+	public void setWindow(JFrame window) {
+		this.window = window;
+	}
+
+
+	public void setMagazine(Magazine magazine) {
+		this.magazine = magazine;
+	}
+	
+	public JFrame getWindow() {
+		return window;
+	}
+
+}	
--- src/main/java/org/team4/view/user/MagazineResultsPanel.java
+++ src/main/java/org/team4/view/user/MagazineResultsPanel.java
@@ -4,38 +4,91 @@ import javax.swing.*;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.Panel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import org.team4.model.items.Book;
+
+import org.team4.maintaindb.MaintainDatabase;
+import org.team4.maintaindb.MaintainMagazine;
 import org.team4.model.items.Magazine;
-import org.team4.view.user.search.BookTableModel;
 import org.team4.view.user.search.MagazineTableModel;
 
 
 public class MagazineResultsPanel extends JPanel {
-    private static final long serialVersionUID = 1L;
-    private static JTable table;
-
-    public MagazineResultsPanel() {
-        setBounds(100, 100, 788, 520);
-        setLayout(null);
-
-        JLabel label = new JLabel("Magazine Search Results ");
-        label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-        label.setBounds(324, 5, 139, 26);
-        add(label);
-
-    }
+	private static final long serialVersionUID = 1L;
+	private static JTable table;
+	private String query;
+	private JFrame window;
 
 
+	public MagazineResultsPanel() {
+		setBounds(100, 100, 788, 520);
+		setLayout(null);
+
+	}
+
+	public MagazineResultsPanel(String query, JFrame window) {
+
+		this.window = window;
+		this.query = query;
+
+		setBounds(100, 100, 788, 520);
+		setLayout(new BorderLayout());
+		JLabel label = new JLabel("Magazine Search Results for Title: " + query);
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		label.setBounds(324, 5, 239, 26);
+		add(label, BorderLayout.PAGE_START);
+
+	}
 
 
-    public MagazineResultsPanel(ArrayList<Magazine> results) {
-        setLayout(new BorderLayout());
-        MagazineTableModel model = new MagazineTableModel(results);
-        table = new JTable(model);
-        add(new JScrollPane(table), BorderLayout.CENTER);
-    }
-    public static JTable getTable() {
-        return table;
-    }
+	public void addSearchResults() {
+		ArrayList<Magazine> results = MaintainDatabase.getInstance().getMagazineDatabase().searchMagazines(query);
+		MagazineTableModel model = new MagazineTableModel(results);
+
+		if(results.isEmpty()) {
+			JOptionPane.showMessageDialog(this, "No results found.", "Search Error", JOptionPane.ERROR_MESSAGE);
+			window.dispose();
+			return;
+		}
+		else {
+			table = new JTable(model);
+			table.setDefaultEditor(Object.class, null);
+			add(new Panel().add(new JScrollPane(table)));
+
+			table.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					int row = table.rowAtPoint(e.getPoint());
+					int col = table.columnAtPoint(e.getPoint());
+					if (col == 0) {
+						Magazine magazine = MaintainMagazine.getInstance().searchExactMagazineByISBN(table.getValueAt(row, 4).toString());
+						JFrame itemInfoFrame = new JFrame(magazine.getTitle());
+						System.out.println(magazine);
+
+						MagazineItemPanel magazinePanel = new MagazineItemPanel(itemInfoFrame, magazine);
+						magazinePanel.showItemInfo();
+						itemInfoFrame.setContentPane(new JScrollPane(magazinePanel));
+						itemInfoFrame.setSize(300, 250);
+						itemInfoFrame.setVisible(true);
+					}
+				}
+			});
+
+
+			window.setContentPane(new JScrollPane(this));
+			window.setSize(1200, 900);
+			window.setVisible(true);
+		}
+
+	}
+
+	public JFrame getWindow() {
+		return window;
+	}
+
+	public static JTable getTable() {
+		return table;
+	}
 }
