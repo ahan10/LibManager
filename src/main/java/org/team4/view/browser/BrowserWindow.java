6252c816f9ad72826c6717ef0169ce1cfea4f847
ahan10 <ahanbhargava10@gmail.com>
2024-03-15 22:18:34 -0400

    [ADD] Added browser window to display the newsletter results

---
+++ src/main/java/org/team4/view/browser/BrowserWindow.java
@@ -0,0 +1,25 @@
+package org.team4.view.browser;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.*;
+
+public class BrowserWindow extends JFrame {
+    public BrowserWindow(String url){
+        setSize(620,440);
+        JScrollPane sp=new JScrollPane();
+        JTextPane tp=new JTextPane();
+        tp.setEditable(false);
+        URL myurl = null;
+        try {
+            myurl=new URL(url);
+        } catch (MalformedURLException ex) {ex.printStackTrace();}
+        sp.setViewportView(tp);
+        add(sp);
+        try {
+            tp.setPage(myurl);
+        } catch (IOException ex) {ex.printStackTrace();}
+    }
+
+}
