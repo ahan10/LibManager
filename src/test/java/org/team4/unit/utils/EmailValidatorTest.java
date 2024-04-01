288a9c5577814b5df94f5045c0756c6cdb727a54
ahan10 <ahanbhargava10@gmail.com>
2024-03-31 20:29:33 -0400

    [ADD] Added tests for EmailValidator

--- src/test/java/org/team4/unit/utils/EmailValidatorTest.java
+++ src/test/java/org/team4/unit/utils/EmailValidatorTest.java
@@ -1,7 +1,59 @@
 package org.team4.unit.utils;
 
+import org.junit.Test;
+import org.team4.utils.EmailValidator;
+
 import static org.junit.Assert.*;
 
 public class EmailValidatorTest {
+    @Test
+    public void test_valid_1(){
+        assertTrue(EmailValidator.validEmail("john.doe@example.com"));
+    }
+
+    @Test
+    public void test_valid_2(){
+        assertTrue(EmailValidator.validEmail("alice_smith1234@gmail.com"));
+    }
+
+    @Test
+    public void test_valid_3(){
+        assertTrue(EmailValidator.validEmail("info@company.com"));
+    }
+
+    @Test
+    public void test_valid_4(){
+        assertTrue(EmailValidator.validEmail("jennifer-123@yahoo.co.uk"));
+    }
+
+    @Test
+    public void test_valid_5(){
+        assertTrue(EmailValidator.validEmail("bob@email.co.jp"));
+    }
+
+    @Test
+    public void test_invalid_1(){
+        assertFalse(EmailValidator.validEmail("john.doe@example"));
+    }
+
+    @Test
+    public void test_invalid_2(){
+        assertFalse(EmailValidator.validEmail("alice.smith@gmail "));
+    }
+
+    @Test
+    public void test_invalid_3(){
+        assertFalse(EmailValidator.validEmail("@company.com"));
+    }
+
+    @Test
+    public void test_invalid_4(){
+        assertFalse(EmailValidator.validEmail("jennifer@123@yahoo.com"));
+    }
+
+    @Test
+    public void test_invalid_5(){
+        assertFalse(EmailValidator.validEmail("bob@email..com"));
+    }
 
 }
