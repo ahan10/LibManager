f82752757327837f6cc7a833d79c3cfbc937f91c
s-w6 <16546401+s-w6@users.noreply.github.com>
2024-03-31 23:01:11 -0400

    [ADD] MaintainBooksTest (Catches not covered)

--- src/test/java/org/team4/unit/maintaindb/MaintainBooksTest.java
+++ src/test/java/org/team4/unit/maintaindb/MaintainBooksTest.java
@@ -2,6 +2,135 @@ package org.team4.unit.maintaindb;
 
 import static org.junit.Assert.*;
 
+import org.junit.*;
+import org.team4.maintaindb.MaintainBooks;
+import org.team4.model.items.Book;
+import org.team4.model.items.Item;
+import org.team4.model.items.builder.BookBuilder;
+
+
 public class MaintainBooksTest {
+	
+	MaintainBooks bookMaintainer;
+	
+	@Before
+	public void setUp() {
+		bookMaintainer = MaintainBooks.getInstance();
+	}
+	
+	@Test
+	public void testFirstBook() {
+		assertEquals("9781234567890", bookMaintainer.getAllBooks().get(0).getISBN());
+	}
+	
+	@Test
+	public void testDecreaseCopies() {
+		Item itemToDecrease = bookMaintainer.getAllBooks().get(3);
+		int newNumberOfCopies = itemToDecrease.getQuantity()-1;
+		bookMaintainer.decreaseNumberOfCopies(itemToDecrease);
+		assertEquals(newNumberOfCopies, bookMaintainer.getAllBooks().get(3).getQuantity());
+	}
+	
+	@Test
+	public void testIncreaseCopies() {
+		Item itemToDecrease = bookMaintainer.getAllBooks().get(3);
+		int newNumberOfCopies = itemToDecrease.getQuantity()+1;
+		bookMaintainer.increaseNumberOfCopies(itemToDecrease);
+		assertEquals(newNumberOfCopies, bookMaintainer.getAllBooks().get(3).getQuantity());
+		
+	}
+	
+	@Test
+	public void testSearchExactBook() {
+		assertTrue(bookMaintainer.searchExactBook("9781234567890"));
+	}
+	
+	@Test
+	public void testSearchExactBookByISBN() {
+		String ISBNToSearch = "9780374533557";
+		Book searchedBook = bookMaintainer.searchExactBookByISBN(ISBNToSearch);
+		assertEquals(ISBNToSearch, searchedBook.getISBN());
+	}
+	
+	@Test
+	public void bookNotFountByISBN() {
+		String ISBNToSearch = "9785374532558";
+		Book searchedBook = bookMaintainer.searchExactBookByISBN(ISBNToSearch);
+		assertNull(searchedBook);
+	}
+	
+	@Test
+	public void testSearchExactBookByTitle() {
+		String titleToSearch = "Pride and Prejudice";
+		Book searchedBook = bookMaintainer.searchExactBookByTitle(titleToSearch);
+		assertEquals(titleToSearch, searchedBook.getTitle());
+	}
+	
+	@Test
+	public void bookNotFountByTitle() {
+		String titleToSearch = "This Book Does Not Exist";
+		Book searchedBook = bookMaintainer.searchExactBookByISBN(titleToSearch);
+		assertNull(searchedBook);
+	}
+	
+	@Test
+	public void testAddBook() {
+		Book newBook = ((BookBuilder) new BookBuilder()
+                .title("This is a test book")
+                .yearPublished(2077)
+                .price(123.45)
+                .ISBN("1234567890123")
+                .quantity(20))
+                .noOfPages(123)
+                .author("Author")
+                .publisher("Publisher")
+                .edition(0)
+                .genre("Genre")
+                .hasHardCopy(false)
+                .hasSoftCopy(false)
+                .build();
+		
+		bookMaintainer.addBook(newBook);
+		assertTrue(bookMaintainer.searchExactBook("1234567890123"));
+	}
+	
+	@Test
+	public void testAddDuplicateBook() {
+		Book newBook = ((BookBuilder) new BookBuilder()
+                .title("This is a test book")
+                .yearPublished(2077)
+                .price(123.45)
+                .ISBN("1234567890123")
+                .quantity(20))
+                .noOfPages(123)
+                .author("Author")
+                .publisher("Publisher")
+                .edition(0)
+                .genre("Genre")
+                .hasHardCopy(false)
+                .hasSoftCopy(false)
+                .build();
+		
+		bookMaintainer.addBook(newBook);
+		assertTrue(bookMaintainer.searchExactBook("1234567890123"));
+	}
+	
+	@Test
+	public void removeTestBook() {
+		int numberOfBooks = bookMaintainer.getAllBooks().size();
+		bookMaintainer.getAllBooks().remove(numberOfBooks-1);
+		assertEquals(numberOfBooks-1, bookMaintainer.getAllBooks().size());
+	}
+	
+	@Test
+	public void testUpdate() {
+		try {
+			bookMaintainer.update();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertFalse(bookMaintainer.searchExactBook("1234567890123"));
+	}
 
 }
