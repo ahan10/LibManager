51882110f7a7137cac35755b8c120a53576f0aee
s-w6 <16546401+s-w6@users.noreply.github.com>
2024-04-03 16:12:45 -0400

    [ADD] remaining maintain notif tests

--- src/test/java/org/team4/unit/maintaindb/MaintainNotificationsTest.java
+++ src/test/java/org/team4/unit/maintaindb/MaintainNotificationsTest.java
@@ -19,48 +19,56 @@ public class MaintainNotificationsTest {
 		String testNotif = "This is a test.@@@@test1@test.com";
 		notificationMaintainer.getNotifications().add(testNotif);
 		assertEquals(1,notificationMaintainer.getNotifications().size());
-		notificationMaintainer.getNotifications().remove(testNotif);
+		notificationMaintainer.getNotifications().clear();
 	}
 	
 	@Test
-	public void updateNotification() {
+	public void updateNotification() throws Exception {
 		String testNotif = "This is a test.@@@@test1@test.com";
 		notificationMaintainer.getNotifications().add(testNotif);
-		try {
-			notificationMaintainer.update();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		notificationMaintainer.update();
 		assertEquals(1,notificationMaintainer.getNotifications().size());
-		notificationMaintainer.getNotifications().remove(testNotif);
-		try {
-			notificationMaintainer.update();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		notificationMaintainer.getNotifications().clear();
+		notificationMaintainer.update();
 	}
 	
 	@Test
-	public void loadNotification() {
+	public void updateMultipleNotification() throws Exception {
+		String testNotif = "This is a test.@@@@test1@test.com";
+		String testNotif2 = "This is a test.@@@@test2@test.com";
+		notificationMaintainer.getNotifications().add(testNotif);
+		notificationMaintainer.getNotifications().add(testNotif2);
+		notificationMaintainer.update();
+		assertEquals(2,notificationMaintainer.getNotifications().size());
+		notificationMaintainer.getNotifications().clear();
+		notificationMaintainer.update();
+	}
+	
+	@Test
+	public void loadNotification() throws Exception {
 		String testNotif = "This is a test.@@@@test1@test.com";
 		notificationMaintainer.getNotifications().add(testNotif);
-		try {
-			notificationMaintainer.update();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
-			notificationMaintainer.load();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		notificationMaintainer.update();
+		notificationMaintainer.getNotifications().clear();
+		notificationMaintainer.load();
 		assertEquals(1,notificationMaintainer.getNotifications().size());
+		notificationMaintainer.getNotifications().clear();
+		notificationMaintainer.update();
+	}
+	
+	@Test
+	public void loadMultipleNotification() throws Exception {
+		String testNotif = "This is a test.@@@@test1@test.com";
+		String testNotif2 = "This is a test.@@@@test1@test.com";
+		notificationMaintainer.getNotifications().add(testNotif);
+		notificationMaintainer.getNotifications().add(testNotif2);
+		notificationMaintainer.update();
+		notificationMaintainer.getNotifications().clear();
+		notificationMaintainer.load();
+		assertEquals(2,notificationMaintainer.getNotifications().size());
 		notificationMaintainer.getNotifications().remove(testNotif);
-		try {
-			notificationMaintainer.update();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		notificationMaintainer.getNotifications().remove(testNotif2);
+		notificationMaintainer.update();
 	}
 	
 	@Test
@@ -79,6 +87,37 @@ public class MaintainNotificationsTest {
 		String[] displayString = notificationMaintainer.getNotifications().get(0).split("@@@@");
 		notificationMaintainer.getNotifications().remove(testNotif);
 		assertEquals("test1@test.com", displayString[1]);
+	}
+	
+	@Test
+	public void addMultipleNotifs() {
+		String testNotif = "This is a test.@@@@test1@test.com";
+		String testNotif2 = "This is a test.@@@@test2@test.com";
+		notificationMaintainer.getNotifications().add(testNotif);
+		notificationMaintainer.getNotifications().add(testNotif2);
+		String[] displayString = notificationMaintainer.getNotifications().get(0).split("@@@@");
+		String[] displayString2 = notificationMaintainer.getNotifications().get(1).split("@@@@");
+		notificationMaintainer.getNotifications().remove(testNotif);
+		notificationMaintainer.getNotifications().remove(testNotif2);
+		assertEquals(displayString2[0], displayString[0]);
+	}
+	
+	@Test
+	public void readNotificationMessage2() {
+		String testNotif = "This is another test.@@@@test2@test.com";
+		notificationMaintainer.getNotifications().add(testNotif);
+		String[] displayString = notificationMaintainer.getNotifications().get(0).split("@@@@");
+		notificationMaintainer.getNotifications().remove(testNotif);
+		assertEquals("This is another test.", displayString[0]);
+	}
+	
+	@Test
+	public void readNotificationSubmitter2() {
+		String testNotif = "This is another test.@@@@test2@test.com";
+		notificationMaintainer.getNotifications().add(testNotif);
+		String[] displayString = notificationMaintainer.getNotifications().get(0).split("@@@@");
+		notificationMaintainer.getNotifications().remove(testNotif);
+		assertEquals("test2@test.com", displayString[1]);
 	}
 
 }
